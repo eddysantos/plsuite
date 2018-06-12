962b04f5f6b74964cd1533c39c68ff498b630f8b
Eduardo Santos <e.santosancira@gmail.com>
2018-06-12 17:10:10 -0500

    Fixed issues with search
    Issues Fixed.

--- Ubicaciones/Viajes/tripSearch.php
+++ Ubicaciones/Viajes/tripSearch.php
@@ -19,7 +19,7 @@ $data = array(
 $bind_values = array();
 
 if (isset($_GET['submit'])) {
-  if (isset($_GET['cTripsTxt'])) {
+  if ($_GET['cTripsTxt'] != "") {
     $data = array(
       'input_f'=>"",
       'input_t'=>"",
@@ -35,10 +35,10 @@ if (isset($_GET['submit'])) {
     }
   } else {
     $data = array(
-      'input_f' => date('Y-m-d', strtotime('last week monday')),
-      'input_t' => date('Y-m-d', strtotime('last week sunday')),
-      'from' => "'" . date('Y-m-d', strtotime($_GET['cTripsFrom'])) . " 00:00'",
-      'to' => "'" . date('Y-m-d', strtotime($_GET['cTripsTo'])) . " 23:59'",
+      'input_f' => date('Y-m-d', strtotime($_GET['cTripsFrom'])),
+      'input_t' => date('Y-m-d', strtotime($_GET['cTripsTo'])),
+      'from' => date('Y-m-d', strtotime($_GET['cTripsFrom'])) . " 00:00",
+      'to' => date('Y-m-d', strtotime($_GET['cTripsTo'])) . " 23:59",
       'txt' => ""
     );
     $where = '(tl.date_arrival >= ? AND tl.date_arrival <= ?)';
@@ -61,7 +61,7 @@ echo "<link rel='stylesheet' href='/plsuite/Resources/CSS/trips.css'>";
 require $root . '/plsuite/Resources/PHP/Utilities/header.php';
 require $root . '/plsuite/Resources/PHP/Utilities/initialScript.php';
 
-$query = "SELECT t.trip_year AS TripYear , t.pkid_trip AS idTrip , t.trip_status AS status , t.date_open AS DateOpen , t.trailer_number AS TrailerNumber , tl.pk_linehaul_number AS linehaul_number , tl.origin_city AS OriginCity , tl.origin_state AS OriginState , tl.destination_city AS DestinationCity , tl.destination_state AS DestinationState , b.brokerName AS broker , tl.linehaul_status AS lh_status , t.pkid_trip AS tripid , tl.pk_idlinehaul AS linehaulid , max(tlm.pkid_movement) AS idMovement , tl.date_departure date_departure , tl.date_arrival date_arrival , tl.date_delivery date_delivery , SUM( CASE tlm.movement_type WHEN 'E' THEN tlm.miles_google ELSE 0 END) AS empty_miles , SUM( CASE tlm.movement_type WHEN 'L' THEN tlm.miles_google ELSE 0 END) AS loaded_miles ,( SELECT CONCAT(d.nameFirst , ' ' , d.nameLast) FROM ct_trip_linehaul_movement tlm LEFT JOIN ct_drivers d ON tlm.fkid_driver = d.pkid_driver WHERE tlm.fkid_linehaul = tl.pk_idlinehaul ORDER BY tlm.pkid_movement DESC LIMIT 1) last_driver FROM ct_trip t LEFT JOIN ct_trip_linehaul tl ON t.pkid_trip = tl.fk_idtrip LEFT JOIN ct_trip_linehaul_movement tlm ON tlm.fkid_linehaul = tl.pk_idlinehaul LEFT JOIN ct_brokers b ON tl.fkid_broker = b.pkid_broker WHERE $where GROUP BY t.trip_year , t.pkid_trip , tl.pk_idlinehaul";
+$query = "SELECT t.trip_year AS TripYear , t.pkid_trip AS idTrip , t.trip_status AS status , t.date_open AS DateOpen , t.trailer_number AS TrailerNumber , tl.pk_linehaul_number AS linehaul_number , tl.origin_city AS OriginCity , tl.origin_state AS OriginState , tl.destination_city AS DestinationCity , tl.destination_state AS DestinationState , b.brokerName AS broker , tl.linehaul_status AS lh_status , tl.trip_rate trip_rate, t.pkid_trip AS tripid , tl.pk_idlinehaul AS linehaulid , max(tlm.pkid_movement) AS idMovement , tl.date_departure date_departure , tl.date_arrival date_arrival , tl.date_delivery date_delivery , SUM( CASE tlm.movement_type WHEN 'E' THEN tlm.miles_google ELSE 0 END) AS empty_miles , SUM( CASE tlm.movement_type WHEN 'L' THEN tlm.miles_google ELSE 0 END) AS loaded_miles ,( SELECT CONCAT(d.nameFirst , ' ' , d.nameLast) FROM ct_trip_linehaul_movement tlm LEFT JOIN ct_drivers d ON tlm.fkid_driver = d.pkid_driver WHERE tlm.fkid_linehaul = tl.pk_idlinehaul ORDER BY tlm.pkid_movement DESC LIMIT 1) last_driver FROM ct_trip t LEFT JOIN ct_trip_linehaul tl ON t.pkid_trip = tl.fk_idtrip LEFT JOIN ct_trip_linehaul_movement tlm ON tlm.fkid_linehaul = tl.pk_idlinehaul LEFT JOIN ct_brokers b ON tl.fkid_broker = b.pkid_broker WHERE $where GROUP BY t.trip_year , t.pkid_trip , tl.pk_idlinehaul";
 
 $stmt = $db->prepare($query);
 if (!($stmt)) {
@@ -113,6 +113,7 @@ if ($rslt->num_rows == 0) {
       $system_callback['rows'][$row['idTrip']]['TripYear'] = $row['TripYear'];
       $system_callback['rows'][$row['idTrip']]['idTrip'] = $row['idTrip'];
       $system_callback['rows'][$row['idTrip']]['status'] = $row['status'];
+      // $system_callback['rows'][$row['idTrip']]['trip_rate'] = $row['trip_rate'];
     }
   }
 }
