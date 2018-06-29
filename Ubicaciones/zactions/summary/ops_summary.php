f53462a8b000b7ee9772dd8f4a3317de4408506f
Eduardo Santos <e.santosancira@gmail.com>
2018-06-29 12:09:10 -0500

    Fixed Home Page
    System was not designed to recognize 0 results on the tables, thus
    failing on the input of data. Included a ‘No info found’ provision.

--- Ubicaciones/zactions/summary/ops_summary.php
+++ Ubicaciones/zactions/summary/ops_summary.php
@@ -10,11 +10,18 @@ $query = "SELECT tl.lh_number linehaul , tl.origin_city o_city , tl.origin_state
 $stmt = $db->prepare($query);
 $stmt->execute();
 $rslt = $stmt->get_result();
+$rows = $rslt->num_rows;
 
-while ($row = $rslt->fetch_assoc()) {
-  $sc['data']['nb_trips']['count']++;
-  $sc['data']['nb_trips']['table'] .= "<tr><td class='fit'>$row[linehaul]</td><td class='fit'>$row[truck]</td><td>$row[d_city], $row[d_state]</td></tr>";
+if ($rows == 0) {
+  $sc['data']['nb_trips']['count'] = 0;
+  $sc['data']['nb_trips']['table'] = "<tr><td colspan='3'>No trips found</td></tr>";
+} else {
+  while ($row = $rslt->fetch_assoc()) {
+    $sc['data']['nb_trips']['count']++;
+    $sc['data']['nb_trips']['table'] .= "<tr><td class='fit'>$row[linehaul]</td><td class='fit'>$row[truck]</td><td>$row[d_city], $row[d_state]</td></tr>";
+  }
 }
+
 
 
 $query = "SELECT tl.lh_number linehaul , tl.origin_city o_city , tl.origin_state o_state , tl.destination_city d_city , tl.destination_state d_state , trk.truckNumber truck FROM ct_trip_linehaul tl LEFT JOIN ct_truck trk ON trk.pkid_truck = tl.current_tractor WHERE tl.date_arrival IS NULL AND tl.fk_idtrip <> '' AND tl.linehaul_status NOT IN('Closed' , 'Cancelled') AND tl.origin_zip <> '78041'";
@@ -22,11 +29,18 @@ $query = "SELECT tl.lh_number linehaul , tl.origin_city o_city , tl.origin_state
 $stmt = $db->prepare($query);
 $stmt->execute();
 $rslt = $stmt->get_result();
+$rows = $rslt->num_rows;
 
-while ($row = $rslt->fetch_assoc()) {
-  $sc['data']['sb_trips']['count']++;
-  $sc['data']['sb_trips']['table'] .= "<tr><td class='fit'>$row[linehaul]</td><td class='fit'>$row[truck]</td><td>$row[o_city], $row[o_state]</td></tr>";
+if ($rows == 0) {
+  $sc['data']['sb_trips']['count'] = 0;
+  $sc['data']['sb_trips']['table'] = "<tr><td colspan='3'>No trips found</td></tr>";
+} else {
+  while ($row = $rslt->fetch_assoc()) {
+    $sc['data']['sb_trips']['count']++;
+    $sc['data']['sb_trips']['table'] .= "<tr><td class='fit'>$row[linehaul]</td><td class='fit'>$row[truck]</td><td>$row[o_city], $row[o_state]</td></tr>";
+  }
 }
+
 
 
 $query = "SELECT t.trip_number trip , t.trailer_number trailer , max(tl.date_departure) departure , max(tl.date_appointment) appointment FROM ct_trip t LEFT JOIN ct_trip_linehaul tl ON t.pkid_trip = tl.fk_idtrip WHERE tl.linehaul_status NOT IN('Closed' , 'Cancelled') GROUP BY t.trip_number HAVING count(tl.fk_idtrip) = 1";
@@ -34,11 +48,18 @@ $query = "SELECT t.trip_number trip , t.trailer_number trailer , max(tl.date_dep
 $stmt = $db->prepare($query);
 $stmt->execute();
 $rslt = $stmt->get_result();
+$rows = $rslt->num_rows;
 
-while ($row = $rslt->fetch_assoc()) {
-  $sc['data']['pr_trips']['count']++;
-  $sc['data']['pr_trips']['table'] .= "<tr><td class='fit'>$row[trip]</td><td class='fit'>$row[trailer]</td><td>$row[appointment]</td></tr>";
+if ($rows == 0) {
+  $sc['data']['pr_trips']['count'] = 0;
+  $sc['data']['pr_trips']['table'] = "<tr><td colspan='3'>No trips found</td></tr>";
+} else {
+  while ($row = $rslt->fetch_assoc()) {
+    $sc['data']['pr_trips']['count']++;
+    $sc['data']['pr_trips']['table'] .= "<tr><td class='fit'>$row[trip]</td><td class='fit'>$row[trailer]</td><td>$row[appointment]</td></tr>";
+  }
 }
+
 
 
 $query = "SELECT tl.lh_number linehaul , t.trailer_number trailer, tl.trip_rate rate , tl.date_end , datediff(date(curdate()) , tl.date_end) days FROM ct_trip_linehaul tl LEFT JOIN ct_trip t ON t.pkid_trip = tl.fk_idtrip WHERE tl.fk_idtrip <> '' AND tl.linehaul_status = 'Closed' AND tl.invoice_number IS NULL";
@@ -46,12 +67,20 @@ $query = "SELECT tl.lh_number linehaul , t.trailer_number trailer, tl.trip_rate 
 $stmt = $db->prepare($query);
 $stmt->execute();
 $rslt = $stmt->get_result();
+$rows = $rslt->num_rows;
 
-while ($row = $rslt->fetch_assoc()) {
-  $sc['data']['pi_trips']['count']++;
-  $sc['data']['pi_trips']['amount'] += $row['rate'];
-  $sc['data']['pi_trips']['table'] .= "<tr><td class='fit'>$row[linehaul]</td><td class='fit'>$row[trailer]</td><td>$row[date_end] ($row[days])</td></tr>";
+if ($rows == 0) {
+  $sc['data']['pi_trips']['count'] = 0;
+  $sc['data']['pi_trips']['amount'] = 0;
+  $sc['data']['pi_trips']['table'] = "<tr><td colspan='3'>No trips found</td></tr>";
+} else {
+  while ($row = $rslt->fetch_assoc()) {
+    $sc['data']['pi_trips']['count']++;
+    $sc['data']['pi_trips']['amount'] += $row['rate'];
+    $sc['data']['pi_trips']['table'] .= "<tr><td class='fit'>$row[linehaul]</td><td class='fit'>$row[trailer]</td><td>$row[date_end] ($row[days])</td></tr>";
+  }
 }
+
 
 $sc['data']['pi_trips']['amount'] = number_format($sc['data']['pi_trips']['amount'], 2);
 
@@ -61,13 +90,21 @@ $query = "SELECT tl.lh_number linehaul , t.trailer_number trailer , tl.trip_rate
 $stmt = $db->prepare($query);
 $stmt->execute();
 $rslt = $stmt->get_result();
+$rows = $rslt->num_rows;
 
-while ($row = $rslt->fetch_assoc()) {
-  $due = date('Y-m-d', strtotime($row['payment_due']));
-  $sc['data']['pp_trips']['count']++;
-  $sc['data']['pp_trips']['amount'] += $row['rate'];
-  $sc['data']['pp_trips']['table'] .= "<tr><td class='fit'>$row[linehaul]</td><td class='fit'>$row[trailer]</td><td>$due ($row[days])</td></tr>";
+if ($rows == 0) {
+  $sc['data']['pp_trips']['count'] = 0;
+  $sc['data']['pp_trips']['amount'] = 0;
+  $sc['data']['pp_trips']['table'] = "<tr><td colspan='3'>No trips found</td></tr>";
+} else {
+  while ($row = $rslt->fetch_assoc()) {
+    $due = date('Y-m-d', strtotime($row['payment_due']));
+    $sc['data']['pp_trips']['count']++;
+    $sc['data']['pp_trips']['amount'] += $row['rate'];
+    $sc['data']['pp_trips']['table'] .= "<tr><td class='fit'>$row[linehaul]</td><td class='fit'>$row[trailer]</td><td>$due ($row[days])</td></tr>";
+  }
 }
+
 
 $sc['data']['pp_trips']['amount'] = number_format($sc['data']['pi_trips']['amount'], 2);
 
