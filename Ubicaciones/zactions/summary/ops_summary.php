c063032b5907ad5b30571ccf4dad0f699394fb2f
Eduardo Santos <e.santosancira@gmail.com>
2018-07-06 15:57:53 -0500

    Invoice_control bugs
    Payment date was not being pulled properly, and number_format for
    Pending Payment trips amount was operating over the formate pending
    invoice trips amount.

--- Ubicaciones/zactions/summary/ops_summary.php
+++ Ubicaciones/zactions/summary/ops_summary.php
@@ -85,7 +85,7 @@ if ($rows == 0) {
 $sc['data']['pi_trips']['amount'] = number_format($sc['data']['pi_trips']['amount'], 2);
 
 
-$query = "SELECT tl.lh_number linehaul , t.trailer_number trailer , tl.trip_rate rate , tl.invoice_payment_due payment_due , datediff( date(curdate()) , tl.invoice_payment_due) days FROM ct_trip_linehaul tl LEFT JOIN ct_trip t ON t.pkid_trip = tl.fk_idtrip WHERE tl.fk_idtrip <> '' AND tl.linehaul_status = 'Closed' AND tl.invoice_payment_due < curdate()";
+$query = "SELECT tl.lh_number linehaul , t.trailer_number trailer , tl.trip_rate rate , tl.invoice_payment_due payment_due , datediff( date(curdate()) , tl.invoice_payment_due) days FROM ct_trip_linehaul tl LEFT JOIN ct_trip t ON t.pkid_trip = tl.fk_idtrip WHERE tl.fk_idtrip <> '' AND tl.linehaul_status = 'Closed' AND tl.invoice_payment_due < curdate() AND tl.invoice_payment_date IS NULL";
 
 $stmt = $db->prepare($query);
 $stmt->execute();
@@ -106,7 +106,7 @@ if ($rows == 0) {
 }
 
 
-$sc['data']['pp_trips']['amount'] = number_format($sc['data']['pi_trips']['amount'], 2);
+$sc['data']['pp_trips']['amount'] = number_format($sc['data']['pp_trips']['amount'], 2);
 
 
 
