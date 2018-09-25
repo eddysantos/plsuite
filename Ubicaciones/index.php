6e7fdfe2a44bd95c0c7222a41f3af8a9d0e32ec1
Eduardo Santos <e.santosancira@gmail.com>
2018-09-25 13:43:29 -0500

    Added navigation pane on dash
    Added navigation pane and proper classes to manage them.

--- Ubicaciones/index.php
+++ Ubicaciones/index.php
@@ -20,16 +20,58 @@ $today = date('m/d/Y', strtotime('today'));
 $sunday_5weeks = date('m/d/Y', strtotime('sunday 5 weeks ago'));
  ?>
 
-
-<div class="container-fluid pt-3" style="margin-top: 50px; height: calc(100vh - 100px)">
-  <div class="row new-rows">
-    <div class="ml-2">
-      <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-        <a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Operations</a>
-        <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Tractors</a>
+<div class="main-details-container">
+  <div class="row">
+    <div class="col-sm-2 ml-0 pl-0 border border-bottom-0 border-left-0 border-top-0 ml-0 pl-0 pr-0">
+      <nav class="nav flex-column" id="dashboard-nav-pane" role="tablist">
+        <a class="nav-link dash active" id="div3-tab" data-toggle="tab" role="tab" aria-selected="true" aria-controls="div3" href="#division3-pane">Division 3</a>
+        <a class="nav-link dash" id="div4-tab" data-toggle="tab" role="tab" aria-selected="false" aria-controls="div4" href="#division4-pane">Division 4</a>
+      </nav>
+    </div>
+    <div class="col-sm-10 tab-info">
+      <div class="tab-content p-2" id="dashboard-tab-content">
+        <div class="tab pane show active" id="division3-pane" role="tabpanel" aria-labelledby="div3-tab">
+          <div class="d-flex justify-content-around">
+            <div class="border p-2 mb-2 border mr-1 w-50" style="height: 325px">
+              <h6>Pending Invoice: <span class="text-secondary" id="pi-count"></span></h6>
+              <table class="table table-striped table-sm dash-table" id="pending-invoice-trips">
+                <thead>
+                  <tr>
+                    <th class="fit">Linehaul</th>
+                    <th class="fit">Trailer</th>
+                    <th>Closed Date (Days)</th>
+                  </tr>
+                </thead>
+                <tbody></tbody>
+              </table>
+            </div>
+            <div class="border p-2 mb-2 border ml-1 w-50">
+              <h6>Pending Payment: <span class="text-secondary" id="pp-count"></span></h6>
+              <table class="table table-striped table-sm dash-table" id="pending-payment-trips">
+                <thead>
+                  <tr>
+                    <th class="fit">Trip</th>
+                    <th class="fit">Trailer</th>
+                    <th>Payment Due Date</th>
+                  </tr>
+                </thead>
+                <tbody></tbody>
+              </table>
+            </div>
+        </div>
       </div>
     </div>
-    <div class="col-11">
+  </div>
+</div>
+<!-- <div class="container-fluid pt-3" style="margin-top: 50px; height: calc(100vh - 100px)">
+  <div class="row">
+    <div class="col">
+      <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
+        <a class="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Division 3</a>
+        <a class="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Division 4</a>
+      </div>
+    </div>
+    <div class="col-10">
       <div class="d-flex justify-content-around">
         <div class="border p-2 mb-2 border mr-1 w-50" style="height: 325px">
           <h6>Northbound En Route: <span class="text-secondary" id="nb-count"></span></h6>
@@ -103,7 +145,7 @@ $sunday_5weeks = date('m/d/Y', strtotime('sunday 5 weeks ago'));
 
     </div>
   </div>
-</div>
+</div> -->
 
 <?php
 require $root . '/plsuite/Resources/PHP/Utilities/footer.php';
