9475a30c32578591061c0e4daed24dc5e2c2f747
Eduardo Santos <e.santosancira@gmail.com>
2018-10-01 12:03:35 -0500

    Adjusted pending-delivery table size (columns)

--- Ubicaciones/zactions/summary/ops_summary.php
+++ Ubicaciones/zactions/summary/ops_summary.php
@@ -109,7 +109,7 @@ if ($rows == 0) {
 $sc['data']['pp_trips']['amount'] = number_format($sc['data']['pp_trips']['amount'], 2);
 
 
-$query = "SELECT tl.lh_number linehaul , t.trailer_number trailer , trk.truckNumber tractor , tl.date_appointment appointment , b.brokerName broker , tl.broker_reference br_reference, tl.origin_city o_city, tl.origin_state o_state FROM ct_trip_linehaul tl LEFT JOIN ct_trip t ON t.pkid_trip = tl.fk_idtrip LEFT JOIN ct_brokers b ON tl.fkid_broker = b.pkid_broker LEFT JOIN ct_truck trk ON trk.pkid_truck = tl.current_tractor WHERE tl.date_delivery IS NULL AND tl.fk_idtrip <> '' AND tl.linehaul_status NOT IN('Closed' , 'Cancelled') AND( tl.origin_zip <> '78041' AND tl.origin_zip <> '78045') ORDER BY appointment ASC";
+$query = "SELECT tl.lh_number linehaul , t.trailer_number trailer , trk.truckNumber tractor , tl.date_appointment appointment , b.brokerName broker , tl.broker_reference br_reference, tl.origin_city o_city, tl.origin_state o_state, datediff(date(curdate()) , tl.date_appointment) days FROM ct_trip_linehaul tl LEFT JOIN ct_trip t ON t.pkid_trip = tl.fk_idtrip LEFT JOIN ct_brokers b ON tl.fkid_broker = b.pkid_broker LEFT JOIN ct_truck trk ON trk.pkid_truck = tl.current_tractor WHERE tl.date_delivery IS NULL AND tl.fk_idtrip <> '' AND tl.linehaul_status NOT IN('Closed' , 'Cancelled') AND( tl.origin_zip <> '78041' AND tl.origin_zip <> '78045') ORDER BY appointment ASC";
 
 $stmt = $db->prepare($query);
 $stmt->execute();
@@ -122,9 +122,19 @@ if ($rows == 0) {
   $sc['data']['pd_trips']['table'] = "<tr><td colspan='6'>No trips found</td><td></td><td></td></tr>";
 } else {
   while ($row = $rslt->fetch_assoc()) {
+    $appt = date('Y-m-d H:i',strtotime($row['appointment']));
+
+
     $sc['data']['pd_trips']['count']++;
     $sc['data']['pd_trips']['amount'] += $row['rate'];
-    $sc['data']['pd_trips']['table'] .= "<tr><td>$row[linehaul]</td><td>$row[trailer]</td><td>$row[tractor]</td><td>$row[broker]</td><td>$row[o_city], $row[o_state]</td><td>$row[appointment]</td></tr>";
+    $sc['data']['pd_trips']['table'] .= "<tr>
+    <td style='width: 80px'>$row[linehaul]</td>
+    <td style='width: 160px'>$row[trailer]</td>
+    <td style='width: 80px'>$row[tractor]</td>
+    <td>$row[broker]</td><td>$row[o_city], $row[o_state]</td>
+    <td>$appt ($row[days])</td>
+    </tr>";
+    // <td><button class='btn btn-outline-success'>Deliver</button></td>
   }
 }
 
