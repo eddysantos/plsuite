e4a9dd6bd436a9c3a1e0d89b320f652eb96c0652
Eduardo Santos <e.santosancira@gmail.com>
2018-10-18 17:44:36 -0500

    Changed API to all API's.

--- Ubicaciones/Viajes/tripDetails.php
+++ Ubicaciones/Viajes/tripDetails.php
@@ -1033,5 +1033,5 @@ require $root . '/plsuite/Resources/PHP/Utilities/footer.php';
  ?>
  <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script> -->
  <script src="/plsuite/Resources/jquery_ui_1_12_1/jquery-ui.min.js" charset="utf-8"></script>
- <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBwOdD2iKFCqP6BWJyRPCL-AxYN4ervUB4" async defer></script>
+ <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDuBCFwHZCWMgyeTJ1MI32sXlGnJtIIsUA" async defer></script>
  <script src="js/tripDetails.js" charset="utf-8"></script>
