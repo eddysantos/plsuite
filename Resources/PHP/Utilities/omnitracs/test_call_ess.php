a64bc7c36a75c9323f8c68b1d8c18fbeae72077e
Eduardo Santos <e.santosancira@gmail.com>
2018-11-20 15:13:55 -0600

    Continued tests for omnitracs web sevices.

--- Resources/PHP/Utilities/omnitracs/test_call_ess.php
+++ Resources/PHP/Utilities/omnitracs/test_call_ess.php
@@ -1,5 +1,7 @@
 <?php
 
+// require $root . '/plsuite/Resources/PHP/Utilities/initialScript.php';
+require '/Applications/MAMP/htdocs/plsuite/Resources/PHP/loginDatabase.php';
 
 class WsseAuthHeader extends SoapHeader {
 
@@ -48,7 +50,7 @@ $omni->__setSoapHeaders(array($wsse_header));
 // );
 $subscriber = array(
   'subscriberId'=>'2',
-  'transactionIdIn'=>0
+  'transactionIdIn'=>170387
 );
 
 // $id = new SoapParam('T049', 'id');
@@ -63,23 +65,50 @@ try {
   die();
 }
 
-$transactions = new SimpleXMLElement($ping->dequeue2Return->transactions);
-
-foreach ($transactions as $transaction) {
-  // var_dump($transaction);
-  // echo $transaction->attribute('ID');
-
-  die();
-}
 
 // foreach ($transactions as $transaction) {
 //   $tractor = $transaction->equipment;
 //   var_dump($tractor);
 // }
 
+$query = "INSERT INTO omni_pos_log(tran_id, tran_ts, driverid1, driverid2, tractor, lat, lon, speed) VALUES (?,?,?,?,?,?,?,?)";
+$insert_pos_log = $db->prepare($query);
 
+if (!$insert_pos_log) {
+  die("Error preparing: " . $db->error);
+}
 
-var_dump($ping->dequeue2Return->transactionIdOut);
+$count = $ping->dequeue2Return->count;
+$final_transaction = $ping->dequeue2Return->transactionIdOut;
+$transactions = new SimpleXMLElement($ping->dequeue2Return->transactions);
+
+echo "Count is: " . $count . "\n";
+echo "Final Transaction is: " . $final_transaction . "\n";
+
+foreach ($transactions as $transaction) {
+  // var_dump($transaction);
+  $position_id =  $transaction->attributes()->ID;
+
+  if ($transaction->{'T.2.12.0'}) {
+    $event_ts = $transaction->{'T.2.12.0'}->eventTS;
+    $tractor = $transaction->{'T.2.12.0'}->equipment->attributes()->ID;
+    $driver = $transaction->{'T.2.12.0'}->driverID;
+    $driver2 = $transaction->{'T.2.12.0'}->driverID2;
+    $lat = $transaction->{'T.2.12.0'}->position->attributes()->lat;
+    $lon = $transaction->{'T.2.12.0'}->position->attributes()->lon;
+    $posTS = $transaction->{'T.2.12.0'}->position->attributes()->posTS;
+    $speed = $transaction->{'T.2.12.0'}->speed;
+  } else {
+    continue;
+  }
+
+  $insert_pos_log->bind_param('ssssssss', $position_id, $event_ts, $driver, $driver2, $tractor, $lat, $lon, $speed);
+  $insert_pos_log->execute();
+
+  if (!$insert_pos_log) {
+    die("Error executing query: " . $insert_pos_log->error);
+  }
+}
 
 // $response = $ping->getVehicleInformationReturn;
 // // var_dump($response);
