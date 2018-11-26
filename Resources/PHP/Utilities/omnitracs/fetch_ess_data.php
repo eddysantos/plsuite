ba14db0ad9564ca2894fd91d2ba4aae3b01ca99a
Eduardo Santos <e.santosancira@gmail.com>
2018-11-26 14:14:25 -0600

    .

--- Resources/PHP/Utilities/omnitracs/fetch_ess_data.php
+++ Resources/PHP/Utilities/omnitracs/fetch_ess_data.php
@@ -101,27 +101,39 @@ do {
 
     switch ($branch) {
       case 'T2120':
-      $event_ts = $transaction->{'T.2.12.0'}->eventTS;
-      $tractor = $transaction->{'T.2.12.0'}->equipment->attributes()->ID;
-      $driver = $transaction->{'T.2.12.0'}->driverID;
-      $driver2 = $transaction->{'T.2.12.0'}->driverID2;
-      $lat = $transaction->{'T.2.12.0'}->position->attributes()->lat;
-      $lon = $transaction->{'T.2.12.0'}->position->attributes()->lon;
-      $posTS = $transaction->{'T.2.12.0'}->position->attributes()->posTS;
-      $speed = $transaction->{'T.2.12.0'}->speed;
-      $heading = $transaction->{'T.2.12.0'}->heading;
+      try {
+        $event_ts = $transaction->{'T.2.12.0'}->eventTS;
+        $tractor = $transaction->{'T.2.12.0'}->equipment->attributes()->ID;
+        $driver = $transaction->{'T.2.12.0'}->driverID;
+        $driver2 = $transaction->{'T.2.12.0'}->driverID2;
+        $lat = $transaction->{'T.2.12.0'}->position->attributes()->lat;
+        $lon = $transaction->{'T.2.12.0'}->position->attributes()->lon;
+        $posTS = $transaction->{'T.2.12.0'}->position->attributes()->posTS;
+        $speed = $transaction->{'T.2.12.0'}->speed;
+        $heading = $transaction->{'T.2.12.0'}->heading;
+      } catch (\Exception $e) {
+        var_dump($e);
+        die();
+      }
+
       break;
 
       case 'T2060':
-      $event_ts = $transaction->{'T.2.06.0'}->eventTS;
-      $tractor = $transaction->{'T.2.06.0'}->equipment->attributes()->ID;
-      $driver = $transaction->{'T.2.06.0'}->driverID;
-      $driver2 = $transaction->{'T.2.06.0'}->driverID2;
-      $lat = $transaction->{'T.2.06.0'}->position->attributes()->lat;
-      $lon = $transaction->{'T.2.06.0'}->position->attributes()->lon;
-      $posTS = $transaction->{'T.2.06.0'}->position->attributes()->posTS;
-      $speed = $transaction->{'T.2.06.0'}->speed;
-      $heading = $transaction->{'T.2.06.0'}->heading;
+      try {
+        $event_ts = $transaction->{'T.2.06.0'}->eventTS;
+        $tractor = $transaction->{'T.2.06.0'}->equipment->attributes()->ID;
+        $driver = $transaction->{'T.2.06.0'}->driverID;
+        $driver2 = $transaction->{'T.2.06.0'}->driverID2;
+        $lat = $transaction->{'T.2.06.0'}->position->attributes()->lat;
+        $lon = $transaction->{'T.2.06.0'}->position->attributes()->lon;
+        $posTS = $transaction->{'T.2.06.0'}->position->attributes()->posTS;
+        $speed = $transaction->{'T.2.06.0'}->speed;
+        $heading = $transaction->{'T.2.06.0'}->heading;
+      } catch (\Exception $e) {
+        var_dump($e);
+        die();
+      }
+
       break;
 
       default:
