811fa28b1045e3dfffbc41a67228fccd95435206
Eduardo Santos <e.santosancira@gmail.com>
2019-01-11 14:19:48 -0600

    Added DB registry for uploaded documents.

--- Ubicaciones/Viajes/actions/upload_file.php
+++ Ubicaciones/Viajes/actions/upload_file.php
@@ -38,26 +38,65 @@ $related_id = $post['id_related'];
 $file_title = $post['identifier'];
 $doc_category = "linehaul";
 
-$system_callback['return'] = [
-  "File name"=>$file_name,
-  "Directory"=>$directory,
-  "Path"=>$path,
-  "added_by"=>$added_by,
-  "related_id"=>$related_id,
-  "file_title"=>$file_title
-];
-
 try {
 
-$system_callback['moved'] = move_uploaded_file($data['file']['tmp_name'], $path);
-$system_callback['message'] = "File uploaded correctly!";
+  $db->begin_transaction();
 
+  $query = "INSERT INTO document_catalog(document_type, document_name, document_url, added_by, fk_related_category) VALUES (?,?,?,?,?)";
+
+  $stmt = $db->prepare($query);
+  if (!($stmt)) {
+    $system_callback['code'] = "500";
+    $system_callback['query'] = $query;
+    $system_callback['message'] = "Error during ADD DOCUMENT REGISTRY query prepare [$stmt->errno]: $stmt->error";
+    exit_script($system_callback);
+  }
+
+  $stmt->bind_param('sssss',
+    $extension,
+    $file_title,
+    $path,
+    $added_by,
+    $related_id
+  );
+  if (!($stmt)) {
+    $system_callback['code'] = "500";
+    $system_callback['query'] = $query;
+    $system_callback['message'] = "Error during ADD DOCUMENT REGISTRY variables binding [$stmt->errno]: $stmt->error";
+    exit_script($system_callback);
+  }
+
+  if (!($stmt->execute())) {
+    $system_callback['code'] = "500";
+    $system_callback['query'] = $query;
+    $system_callback['message'] = "Error during ADD DOCUMENT REGISTRY query execution [$stmt->errno]: $stmt->error";
+    exit_script($system_callback);
+  }
+
+  if ($db->affected_rows == 0) {
+    $system_callback['code'] = $db->error;
+    $system_callback['message'] = "Unable to modify trip to include the first and last movement.";
+    exit_script($system_callback);
+  }
+
+  $moved = move_uploaded_file($data['file']['tmp_name'], $path);
+  if (!$moved) {
+    throw new \Exception("Error moving file.", 1);
+
+  }
+  $system_callback['message'] = "File uploaded correctly!";
+  $system_callback['code'] = "1";
+
+  $db->commit();
 } catch (\Exception $e) {
-
-$system_callback['code'] = "500";
-$system_callback['message'] = "Unable to upload file: $e";
+  $db->rollback();
+  $system_callback['code'] = "500";
+  $system_callback['message'] = "Unable to upload file: $db->error";
+  $system_callback['error'] = "Unable to upload file: $e";
 
 }
+
+
 
 
 exit_script($system_callback);
