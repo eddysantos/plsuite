4459133b878933d67e00820ea4666af0ed4437d9
Eduardo Santos <e.santosancira@gmail.com>
2019-01-11 15:58:33 -0600

    .

--- Ubicaciones/Viajes/actions/showDocumentOnline.php
+++ Ubicaciones/Viajes/actions/showDocumentOnline.php
@@ -6,18 +6,94 @@ $key =hash('sha256', "ewgdhfjjluo3pip4l");
 $iv = substr(hash('sha256', "sdfkljsadf567890saf"), 0, 16);
 $file_id = openssl_decrypt(base64_decode($valor1),$cipher, $key, 0, $iv);
 
-echo $file_id;
-//
-// $url ="https://yourFile.pdf";
-// $content = file_get_contents($url);
-//
-// header('Content-Type: application/pdf');
-// header('Content-Length: ' . strlen($content));
-// header('Content-Disposition: inline; filename="YourFileName.pdf"');
-// header('Cache-Control: private, max-age=0, must-revalidate');
-// header('Pragma: public');
-// ini_set('zlib.output_compression','0');
+$root = $_SERVER['DOCUMENT_ROOT'];
+require $root . '/plsuite/Resources/PHP/Utilities/initialScript.php';
 
-// die($content);
+$sc = [];
+$data = $_POST;
+
+function parseDate($datestamp, $option = 1){
+  if ($datestamp == "") {
+    return $return;
+  }
+
+  if ($option == 1) {
+    $return = date('Y/m/d', strtotime($datestamp));
+    return $return;
+  } else {
+    $return = array(
+      'date'=>"",
+      'time'=>array(
+        'hour'=>"",
+        'minute'=>""
+      )
+    );
+
+    $return['date'] = date('Y-m-d', strtotime($datestamp));
+    $return['time']['hour'] = date('H', strtotime($datestamp));
+    $return['time']['minute'] = date('i', strtotime($datestamp));
+
+    return $return;
+  }
+}
+
+function numberify($number){
+  return number_format($number, 2);
+}
+
+function encrypt($string){
+  $cipher = "AES-256-CBC";
+  $key =hash('sha256', "ewgdhfjjluo3pip4l");
+  $iv = substr(hash('sha256', "sdfkljsadf567890saf"), 0, 16);
+  $token = openssl_encrypt($string, $cipher, $key, 0, $iv);
+  $token = base64_encode($token);
+
+  return $token;
+  // $token = openssl_decrypt(base64_decode("UmhaN284bEUxeStZWXF0eTJ3ODhNQT09"),$cipher, $key, 0, $iv);
+}
+
+
+
+$query = "SELECT document_url FROM document_catalog WHERE pk_document_id = ?";
+
+$stmt = $db->prepare($query);
+if (!($stmt)) {
+  $sc['code'] = "500";
+  $sc['message'] = "Error during query prepare [$db->errno]: $db->error";
+  exit_script($sc);
+}
+
+$stmt->bind_param('s', $file_id);
+if (!($stmt)) {
+  $sc['code'] = "500";
+  $sc['message'] = "Error during variables binding [$stmt->errno]: $stmt->error";
+  exit_script($sc);
+}
+
+if (!($stmt->execute())) {
+  $sc['code'] = "500";
+  $sc['message'] = "Error during query execution [$db->errno]: $db->error";
+  exit_script($sc);
+}
+
+$url = $stmt->get_result()->fetch_assoc();
+
+
+
+$sc['code'] = 1;
+$sc['message'] = "Script called successfully!";
+exit_script($sc);
+
+// $url ="https://yourFile.pdf";
+$content = file_get_contents($url);
+
+header('Content-Type: application/pdf');
+header('Content-Length: ' . strlen($content));
+header('Content-Disposition: inline; filename="YourFileName.pdf"');
+header('Cache-Control: private, max-age=0, must-revalidate');
+header('Pragma: public');
+ini_set('zlib.output_compression','0');
+
+die($content);
 
  ?>
