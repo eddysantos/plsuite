3efb0075cbd0b1ae59c0ec8eca530c92591ff8b5
Eduardo Santos <e.santosancira@gmail.com>
2019-01-11 16:25:33 -0600

    Fixed $file_name -> $filename.

--- Ubicaciones/Viajes/actions/showDocumentOnline.php
+++ Ubicaciones/Viajes/actions/showDocumentOnline.php
@@ -85,7 +85,7 @@ $content = file_get_contents($url);
 
 header('Content-Type: application/pdf');
 header('Content-Length: ' . strlen($content));
-header('Content-Disposition: inline; filename="'.$file_name.'"');
+header('Content-Disposition: inline; filename="'.$filename.'"');
 header('Cache-Control: private, max-age=0, must-revalidate');
 header('Pragma: public');
 ini_set('zlib.output_compression','0');
