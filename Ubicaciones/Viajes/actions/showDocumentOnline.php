821b854b2d507511fe1857fb0ef9c5e1efe24220
Eduardo Santos <e.santosancira@gmail.com>
2019-01-11 15:30:54 -0600

    .

---
+++ Ubicaciones/Viajes/actions/showDocumentOnline.php
@@ -0,0 +1,23 @@
+<?php
+
+$valor1 = $_GET['id'];
+$cipher = "AES-256-CBC";
+$key =hash('sha256', "ewgdhfjjluo3pip4l");
+$iv = substr(hash('sha256', "sdfkljsadf567890saf"), 0, 16);
+$file_id = openssl_decrypt(base64_decode("UmhaN284bEUxeStZWXF0eTJ3ODhNQT09"),$cipher, $key, 0, $iv);
+
+echo $file_id;
+//
+// $url ="https://yourFile.pdf";
+// $content = file_get_contents($url);
+//
+// header('Content-Type: application/pdf');
+// header('Content-Length: ' . strlen($content));
+// header('Content-Disposition: inline; filename="YourFileName.pdf"');
+// header('Cache-Control: private, max-age=0, must-revalidate');
+// header('Pragma: public');
+// ini_set('zlib.output_compression','0');
+
+// die($content);
+
+ ?>
