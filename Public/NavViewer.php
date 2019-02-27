102a123b146fcd05b359116b3aac577b3b0dec7d
Eduardo Santos <e.santosancira@gmail.com>
2019-02-27 11:41:02 -0600

    Started NavViewer
    Navviewer started.

---
+++ Public/NavViewer.php
@@ -0,0 +1,17 @@
+
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: EduardoSantos
+ * Date: 2019-02-27
+ * Time: 11:27
+ */
+
+/*
+ * This file will show the user the current location of the tractor associated with their trip.
+ * */
+
+
+?>
+
