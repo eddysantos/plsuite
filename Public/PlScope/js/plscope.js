3359eccb4df0687356b126e28e584188e0cbd223
Eduardo Santos <e.santosancira@gmail.com>
2019-03-13 16:29:33 -0500

    Continuous changes.

--- Public/PlScope/js/plscope.js
+++ Public/PlScope/js/plscope.js
@@ -83,7 +83,7 @@ $(document).ready(function(){
 
           distanceMatrix.getDistanceMatrix({
             origins: [latlng],
-            destinations: ['zip' + r.data.dzip],
+            destinations: ['zip ' + r.data.dzip],
             travelMode: 'DRIVING',
             avoidHighways: false,
             avoidTolls: true,
@@ -123,7 +123,7 @@ $(document).ready(function(){
 
         directionsService.route({
           origin: latlng,
-          destination: 'zip' + r.data.dzip,
+          destination: 'zip ' + r.data.dzip, //This line should have a space after zip...
           travelMode: 'DRIVING'
         }, function(r_ds,s){
           // console.log(r);
