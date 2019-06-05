b1cb067dc69a84115f772bd47dd7390230f62e46
Eduardo Santos <e.santosancira@gmail.com>
2019-06-05 11:56:48 -0500

    Changed Swal8 name to identify conflict.

--- Resources/PHP/Utilities/footer.php
+++ Resources/PHP/Utilities/footer.php
@@ -26,7 +26,7 @@ require $root . "/plsuite/Resources/PHP/modales/modalCerrarSesion.php";
 
   <script src="/plsuite/Resources/JQuery/jquery-3.2.1.min.js" charset="utf-8"></script>
   <script src="/plsuite/Resources/swal/swal.min.js" charset="utf-8"></script>
-  <script src="/plsuite/Resources/swal8/swal.min.js" charset="utf-8"></script>
+  <script src="/plsuite/Resources/swal8/swal8.min.js" charset="utf-8"></script>
   <script src="/plsuite/Resources/alertify/alertify.min.js" charset="utf-8"></script>
   <!--script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script-->
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
