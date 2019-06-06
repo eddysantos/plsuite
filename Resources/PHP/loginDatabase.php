1a51d6b34a8ca424822fde3000ed87d770089f5c
Eduardo Santos <e.santosancira@gmail.com>
2019-06-05 20:29:15 -0500

    added login database

---
+++ Resources/PHP/loginDatabase.php
@@ -0,0 +1,26 @@
+<?php
+
+/** PRODUCTION DATABASE **/
+// $datab = 'plsuite';
+// $host = '10.1.4.10';
+// $port = 3306;
+// $usr = 'prolog';
+// $pwd = 'f4Tnps.03';
+
+/** TEST DATABASE **/
+$datab = 'plsuite';
+$host = '127.0.0.1';
+$port = 8889;
+$usr = 'root';
+$pwd = 'root';
+
+/** PRODUCTION DATABASE REMOTE**/
+// $datab = 'plsuite';
+// $host = '12.34.236.230';
+// $port = 3306;
+// $usr = 'prolog';
+// $pwd = 'f4Tnps.03';
+
+$db = new mysqli($host, $usr, $pwd, $datab, $port) or die ('Could not connect to the database server ' . $login->error );
+
+ ?>
