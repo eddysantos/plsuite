a08d8c655ddb28e6a29df3559dac397a1a2f4cca
Eduardo Santos <e.santosancira@gmail.com>
2019-07-16 17:09:21 -0500

    Fix truck table not showing.

--- Ubicaciones/Trucks/dashboard.php
+++ Ubicaciones/Trucks/dashboard.php
@@ -8,6 +8,7 @@ $cajas_active = "";
 
 $root = $_SERVER['DOCUMENT_ROOT'];
 require $root . '/plsuite/Resources/PHP/Utilities/session.php';
+require $root . '/plsuite/Resources/PHP/Utilities/initialScript.php';
 require $root . '/plsuite/Resources/PHP/Utilities/header.php';
  ?>
 <div class="container-fluid align-items-center justify-content-between d-flex mb-3" style="margin-top: 70px">
