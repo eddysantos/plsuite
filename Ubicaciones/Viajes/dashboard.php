c3a3e8e23be56b136005c3c4def0c50bb0b60b7c
Eduardo Santos <e.santosancira@gmail.com>
2020-02-21 11:49:33 -0600

    Final fix.

--- Ubicaciones/Viajes/dashboard.php
+++ Ubicaciones/Viajes/dashboard.php
@@ -43,8 +43,8 @@ if ($rslt->num_rows == 0) {
     if ($row['lh_status'] != "Cancelled") {
       $system_callback['rows'][$row['idTrip']]['miles'] += $row['empty_miles'];
       $system_callback['rows'][$row['idTrip']]['miles'] += $row['loaded_miles'];
+      $system_callback['rows'][$row['idTrip']]['rate'] += $row['rate'];
     }
-    $system_callback['rows'][$row['idTrip']]['rate'] += $row['rate'];
     $system_callback['rows'][$row['idTrip']]['rpm'] = number_format(round($system_callback['rows'][$row['idTrip']]['rate']/$system_callback['rows'][$row['idTrip']]['miles'], 2),2);
     foreach ($row as $key => $value) {
       $system_callback['rows'][$row['idTrip']][$row['linehaulid']][$key] = $value;
